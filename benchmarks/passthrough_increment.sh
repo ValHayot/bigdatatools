3a3656747d9f01e620a72a1a34b6e39c30a02ce2
valhayot <valeriehayot@gmail.com>
2020-03-08 04:14:02 +0100

    some minor fixes

--- benchmarks/passthrough_increment.sh
+++ benchmarks/passthrough_increment.sh
@@ -14,26 +14,28 @@ increment() {
 }
 
 passthrough="/root/fuse/build/example/passthrough_fh"
+# its=10
+its=20
 
 fuse_mount="/root/mount"
 cp -r inc_in /dev/shm/
 mkdir /dev/shm/out
-increment native-mem /dev/shm/inc_in/inc_1.nii /dev/shm/out 10 0
+increment native-mem /dev/shm/inc_in/inc_1.nii /dev/shm/out ${its} 0
 rm /dev/shm/out/*
 
 ${passthrough} $fuse_mount
-increment fuse-mem "${fuse_mount}/dev/shm/inc_in/inc_1.nii" "${fuse_mount}/dev/shm/out" 10 0
+increment fuse-mem "${fuse_mount}/dev/shm/inc_in/inc_1.nii" "${fuse_mount}/dev/shm/out" ${its} 0
 fusermount -u ${fuse_mount}
 rm -rf /dev/shm/out/
 rm -rf /dev/shm/inc_in
 
 cp -r inc_in /root
 mkdir /root/out
-increment native-ssd /root/inc_in/inc_1.nii /root/out 10 0
+increment native-ssd /root/inc_in/inc_1.nii /root/out ${its} 0
 rm /root/out/*
 
 ${passthrough} $fuse_mount
-increment fuse-ssd "${fuse_mount}/root/inc_in/inc_1.nii" "${fuse_mount}/root/out" 10 0
+increment fuse-ssd "${fuse_mount}/root/inc_in/inc_1.nii" "${fuse_mount}/root/out" ${its} 0
 fusermount -u ${fuse_mount}
 rm -rf /root/out/
 rm -rf /root/inc_in
