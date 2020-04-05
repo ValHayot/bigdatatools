b5b31a71f84f3ea2cad714a78f4840bfcd2088b6
valhayot <valeriehayot@gmail.com>
2020-04-05 18:43:41 -0400

    added disk benchmark

--- benchmarks/disk/scripts/bench_disks.sh
+++ benchmarks/disk/scripts/bench_disks.sh
@@ -18,3 +18,5 @@ cached_out=$((dd if=$device of=/dev/null bs=1M count=1024 status=progress) 2>&1)
 cached_bw=`echo ${cached_out} | awk '{print $(NF-1),",",$NF}' | tr -d ' '`
 echo ${device},cached,${cached_bw} >> ${bench_file}
 
+rm $device
+
--- benchmarks/disk/scripts/run_benchmarks.py
+++ benchmarks/disk/scripts/run_benchmarks.py
@@ -5,14 +5,17 @@ import sys
 import subprocess
 
 script = sys.argv[1]
-iterations = 10
+benchmark_file = sys.argv[2]
+iterations = sys.argv[3]
 
-devices = ['/dev/shm/benchfile.txt', '/tmp/benchfile.txt', '/mnt/lustre/vhs/benchfile']
+devices = ['/dev/shm/benchfile.txt', '/tmp/benchfile.txt', '/mnt/lustre/vhs/benchfile.txt']
 rand_device = devices*10
 random.shuffle(rand_device)
 
+with open(benchmark_file, 'w+') as f:
+    f.write('device,mode,bandwidth,unit\n')
+
 for dev in rand_device:
-    p = subprocess.Popen([script, dev], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen([script, dev, benchmark_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (out, err) = p.communicate()
-    print(str(out))
 
